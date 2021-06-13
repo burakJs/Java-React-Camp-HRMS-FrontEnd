113fb356b335ab616c5ec73fad7dc4438607c16b
burakJs <burakimdat13@gmail.com>
2021-06-13 04:31:49 +0300

    feat: Added experienceService

---
+++ src/services/experienceService.js
@@ -0,0 +1,17 @@
+export default class ExperienceService {
+  getAll() {
+    return axios.get("http://localhost:8080/api/experience/getAll");
+  }
+
+  add(entryDate, jobPosId, portfolioId, quitDate, workPlaceName) {
+    return axios.post("http://localhost:8080/api/experience/add", {
+      entryDate: entryDate,
+      jobPosition: {
+        id: jobPosId,
+      },
+      portfolioId: portfolioId,
+      quitDate: quitDate,
+      workPlaceName: workPlaceName,
+    });
+  }
+}
